8246ab530c9c895836dc4ec5c95ede9c5ef3d0d4
DeathByDusk <elijahkanellakis@gmail.com>
2021-10-21 21:47:51 -0500

    syncronized user submitted data to be passed between pages and correct repository issues to be displayed when a repository is selected

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,13 +1,29 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
-function gettRepoIssues(repo) {
+function getRepoName() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
+
+function getRepoIssues(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-    fetch(apiUrl).then(function(response) {
+    fetch(apiUrl).then(function (response) {
         // request was successful
         if (response.ok) {
-            response.json().then(function(data) {
+            response.json().then(function (data) {
                 // pass response data to dom function
                 displayIssues(data);
 
@@ -16,9 +32,10 @@ function gettRepoIssues(repo) {
                     displayWarning(repo);
                 }
             });
-        } 
+        }
         else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -72,4 +89,4 @@ function displayWarning(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-gettRepoIssues("facebook/react");
+getRepoName();
